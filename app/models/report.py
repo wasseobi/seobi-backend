e6192ac72f09c257c5561a52b6911c9d0886f459
jooeun921 <jooeun921@gmail.com>
2025-05-28 09:57:38 +0900

    🔧update: create interest table, fix report table

--- app/models/report.py
+++ app/models/report.py
@@ -1,13 +1,13 @@
 from app.models.db import db
 import uuid
-from sqlalchemy.dialects.postgresql import UUID, ENUM
+from sqlalchemy.dialects.postgresql import UUID, ENUM, JSON
 
 class Report(db.Model):
     __tablename__ = 'report'
     id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     user_id = db.Column(UUID(as_uuid=True), db.ForeignKey('user.id'), nullable=False)
     schedule_id = db.Column(UUID(as_uuid=True), db.ForeignKey('schedule.id'), nullable=False)
-    content = db.Column(db.Text, nullable=True)
+    content = db.Column(JSON, nullable=False)
     type = db.Column(ENUM('daily', 'weekly', 'monthly', name='report_type_enum'), nullable=False)
 
     # 관계 설정
