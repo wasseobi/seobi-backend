91a9e989e8ecc989318192da72e9aef3eb6431b2
gideok Kim <ikid4127@naver.com>
2025-05-30 13:13:12 +0900

    🐛 Fix recursive reference

--- app/dao/session_dao.py
+++ app/dao/session_dao.py
@@ -22,7 +22,7 @@ class SessionDAO(BaseDAO[Session]):
         return super().create(user_id=user_id)
     
     def update(self, session_id: uuid.UUID, **kwargs) -> Optional[Session]:
-        return self.update(str(session_id), **kwargs)
+        return super().update(str(session_id), **kwargs)
 
     # NOTE(GideokKim): `update` method를 써도 되지만 타입 안전성, 명확성, 유지보수성 등을 위해 사용함.
     def update_finish_time(self, session_id: uuid.UUID, finish_time: datetime) -> Optional[Session]:
