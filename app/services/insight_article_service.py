6d3637c8c2a2ce73434a0c05cb7840dc16e2793e
Park Ju Hyeong <owo__omo@naver.com>
2025-05-31 22:37:37 +0900

    Reapply "🛠 Fix insight_article_dao 변수명 변경에 따른
    This reverts commit 3fe08392b6e279eba5259f93773885bcc48f3f69.

--- app/services/insight_article_service.py
+++ app/services/insight_article_service.py
@@ -1,4 +1,5 @@
 from app.dao.insight_article_dao import InsightArticleDAO
+from app.dao.base import BaseDAO
 from app.services.interest_service import InterestService
 from app.utils.openai_client import get_openai_client, get_completion
 from app.utils.prompt.service_prompts import INSIGHT_ARTICLE_SYSTEM_PROMPT
@@ -9,6 +10,7 @@ import json
 
 class InsightArticleService:
     def __init__(self):
+        self.base_dao = BaseDAO
         self.insight_article_dao = InsightArticleDAO()
         self.interest_service = InterestService()
 
@@ -58,4 +60,5 @@ class InsightArticleService:
         return article
 
     def delete_article(self, article_id):
-        return self.insight_article_dao.delete(article_id)
+        # NOTE: insight_article_dao에서 delete 삭제하고 (BaseDAO delete 사용하기로 함)
+        return self.base_dao.delete(article_id)
