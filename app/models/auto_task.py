a66a7fd91e4fd9e7952548e43ee36892511d16f4
Park Ju Hyeong <owo__omo@naver.com>
2025-06-01 17:04:38 +0900

    Revert "📤 update: DB"
    This reverts commit 6659d15be8d9ad3b06d904a37b54a88dbefb8c00.

--- app/models/auto_task.py
+++ app/models/auto_task.py
@@ -10,11 +10,11 @@ class AutoTask(db.Model):
     title = db.Column(db.Text, nullable=False)
     repeat = db.Column(db.Text, nullable=True)  # cron 표현식 등 반복 일정 관리
     created_at = db.Column(db.DateTime(timezone=True), default=lambda: datetime.now(timezone.utc), nullable=False)
-    start_at = db.Column(db.DateTime(timezone=True), nullable=True)
-    finish_at = db.Column(db.DateTime(timezone=True), nullable=True)
+    start_at = db.Column(db.DateTime(timezone=True), nullable=False)  # 시작 시간
+    finish_at = db.Column(db.DateTime(timezone=True), nullable=False)  # 종료 시간
     tool = db.Column(db.Text, nullable=False)
     status = db.Column(ENUM('undone', 'done', name='auto_task_status_enum'), nullable=False, default='undone')
-    linked_service = db.Column(db.Text, nullable=True, default=None)  # 외부 연동 서비스명 등
+    linked_service = db.Column(db.Text, nullable=False)  # 외부 연동 서비스명 등
 
     # 관계 설정 (User와 연결)
     user = db.relationship('User', back_populates='auto_tasks')
