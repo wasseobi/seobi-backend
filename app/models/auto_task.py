4195828dc29fa098e2d9fdca630ede88d8f468ee
Park Ju Hyeong <owo__omo@naver.com>
2025-06-01 17:25:51 +0900

    📤 reupdate: DB auto_task table 수정 linked_service default=None 삭제

--- app/models/auto_task.py
+++ app/models/auto_task.py
@@ -14,7 +14,7 @@ class AutoTask(db.Model):
     finish_at = db.Column(db.DateTime(timezone=True), nullable=True)
     tool = db.Column(db.Text, nullable=False)
     status = db.Column(ENUM('undone', 'done', name='auto_task_status_enum'), nullable=False, default='undone')
-    linked_service = db.Column(db.Text, nullable=True, default=None)  # 외부 연동 서비스명 등
+    linked_service = db.Column(db.Text, nullable=True)  # 외부 연동 서비스명 등
 
     # 관계 설정 (User와 연결)
     user = db.relationship('User', back_populates='auto_tasks')
