d87c3b8747c272e7718e86196cce5acc83e4e540
jooeun921 <jooeun921@gmail.com>
2025-06-04 15:53:44 +0900

    ❎ fix: auto_task 복구

--- app/models/auto_task.py
+++ app/models/auto_task.py
@@ -7,6 +7,16 @@ class AutoTask(db.Model):
     __tablename__ = 'auto_task'
     id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     user_id = db.Column(UUID(as_uuid=True), db.ForeignKey('user.id'), nullable=False)
+    title = db.Column(db.Text, nullable=False) # 업무 제목
+    description = db.Column(db.Text, nullable=True) # 업무 설명
+    task_list = db.Column(db.JSON, nullable=True) # 종속성이 있는 업무 목록(JSON)
+    repeat = db.Column(db.Text, nullable=True)  # cron 표현식 등 반복 일정 관리
+    created_at = db.Column(db.DateTime(timezone=True), default=lambda: datetime.now(timezone.utc), nullable=False)
+    start_at = db.Column(db.DateTime(timezone=True), nullable=True)
+    finish_at = db.Column(db.DateTime(timezone=True), nullable=True)
+    tool = db.Column(db.Text, nullable=False)
+    status = db.Column(ENUM('undone', 'done', name='auto_task_status_enum'), nullable=False, default='undone')
+    linked_service = db.Column(db.Text, nullable=True)  # 외부 연동 서비스명 등
 
     # 관계 설정 (User와 연결)
     user = db.relationship('User', back_populates='auto_tasks')
