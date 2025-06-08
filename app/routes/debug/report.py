369a4c6e3b7e27815eec9eee1d8ce7cf82ac3503
gideok Kim <ikid4127@naver.com>
2025-06-08 16:09:17 +0900

    🎨 Change route path

--- app/routes/debug/report.py
+++ app/routes/debug/report.py
@@ -2,7 +2,7 @@ from flask_restx import Namespace, Resource, fields
 from app.services.reports.report_service import ReportService
 from app.utils.auth_middleware import require_auth
 
-ns = Namespace('report', description='리포트 API')
+ns = Namespace('debug/report', description='리포트 API')
 
 report_model = ns.model('Report', {
     'id': fields.String,
